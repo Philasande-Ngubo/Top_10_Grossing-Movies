3d219f6ded3bbb417f9aad7cb8b180aed8cafc60
Philasande  Ngubo <philasande@tocolite.co.za>
2026-02-20 15:21:31 +0200

    made a card that matches the details

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,6 +5,10 @@ export default function MovieCard(){
                 <img  className="movie-image" src="https://m.media-amazon.com/images/M/MV5BMGFlMTVkMDktZGMzMC00Yjk4LWFmNzEtNTFmMzM2YzM3MWFkXkEyXkFqcGc@._V1_.jpg" alt="Movie" />
                 </div>
             <h2>Ziningi izihloko ezikhona empilweni </h2>
+            <div className="textDes">Ratings: <b>6.3</b></div>
+            <div className="textDes">Year: <b>2026</b></div>
+            <div className="textDes" > Genres: <b>bam bam</b></div>
+            <div className="textDes" >Plot: <b>A passionate and tumultuous love story set against the backdrop of the Yorkshire moors, exploring the intense and destructive relationship between Heathcliff and Catherine Earnshaw.</b></div>
             </section>
     )
 }
