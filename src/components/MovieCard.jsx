0fedac6781f6281126298fb4a8118542b17a5ab5
Philasande  Ngubo <philasande@tocolite.co.za>
2026-02-20 15:05:51 +0200

    Added the heading

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,6 +4,7 @@ export default function MovieCard(){
             <div className="imgFrame">
                 <img  className="movie-image" src="https://m.media-amazon.com/images/M/MV5BMGFlMTVkMDktZGMzMC00Yjk4LWFmNzEtNTFmMzM2YzM3MWFkXkEyXkFqcGc@._V1_.jpg" alt="Movie" />
                 </div>
+            <h2>Ziningi izihloko ezikhona empilweni </h2>
             </section>
     )
 }
