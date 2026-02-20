c7872da66888f2d54ad826d1c54612eddc1b7a8a
Philasande  Ngubo <philasande@tocolite.co.za>
2026-02-20 13:58:20 +0200

    finished with the header and it's components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import Logo from "../assets/logo.png"
+export default function Header(){
+    return (
+        <header>
+            <img src={Logo} alt="Logo" />
+            <h2>by Philasande</h2>
+         </header>
+    )
+}
