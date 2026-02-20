388963550e41d1a33915a4fddd530a60237daaed
Philasande  Ngubo <philasande@tocolite.co.za>
2026-02-20 14:46:44 +0200

    Has now the default card now can take images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header.jsx"
+import MovieCard from "./components/MovieCard.jsx"
 function App() {
 
   return (
     <>
       <Header/>
       <main>
-
+        <MovieCard></MovieCard>
       </main>
       </>
   )
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,9 @@
+export default function MovieCard(){
+    return (
+        <section className="movie-card">
+            <div className="imgFrame">
+                <img  className="movie-image" src="https://m.media-amazon.com/images/M/MV5BMGFlMTVkMDktZGMzMC00Yjk4LWFmNzEtNTFmMzM2YzM3MWFkXkEyXkFqcGc@._V1_.jpg" alt="Movie" />
+                </div>
+            </section>
+    )
+}
