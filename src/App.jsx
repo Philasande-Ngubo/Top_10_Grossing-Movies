551470d80eabdd47e521671d0f13ef9398972f07
Philasande  Ngubo <philasande@tocolite.co.za>
2026-02-23 09:46:51 +0200

    added all the neccessary styling mechanisms to process the movie card

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,36 @@ import Header from "./components/Header.jsx"
 import MovieCard from "./components/MovieCard.jsx"
 function App() {
 
+  console.log( fetchTitles())
+
   return (
     <>
       <Header/>
       <main>
         <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
-        <MovieCard></MovieCard>
+       
       </main>
       </>
   )
+}
+
+async function fetchTitles() {
+  const url = 'https://api.imdbapi.dev/titles'; // Example API endpoint
+  
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const dataVariable = await response.json(); 
+    
+    console.log(dataVariable); 
+    return dataVariable;
+
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
 }
 
 export default App
