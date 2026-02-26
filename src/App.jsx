c77a118c28b4b01a6972fe3fdeed96c725b406fa
Philasande  Ngubo <philasande@tocolite.co.za>
2026-02-26 14:35:21 +0200

    Added stars without color thought I was done, but there is no end to passion when you like something its like that at times

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     }
 
     const dataVariable = await response.json(); 
-    console.log(dataVariable)
+
     setData(dataVariable.titles); 
     return dataVariable;
 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,16 @@
 export default function MovieCard(props){
+
+    const isNumeric = (str) => {
+    if (typeof str !== 'string' || str.trim() === '') {
+        return false;
+    }
+  
+    return Number.isFinite(+str);
+    };
+
+
+    const stars = "★ ".repeat( Math.round(props.ratings/2))
+    
     return (
         <a 
         href={"https://www.youtube.com/results?search_query="+props.title.replaceAll(" ","+")+"+Trailer"} 
@@ -9,7 +21,7 @@ export default function MovieCard(props){
                 <img  className="movie-image" src={props.imgSrc} alt={props.title+" image"} />
                 </div>
             <h2>{props.title} </h2>
-            <div className="textDes">Ratings: <b>{props.ratings}</b></div>
+            <div className="textDes">Ratings: <b><span className="stars">{stars}</span>{+ props.ratings}  </b></div>
             <div className="textDes">Year: <b>{props.year}</b></div>
             <div className="textDes" > Genres: <b>{props.type}</b></div>
             <div className="textDes" >Plot: <b>{props.plot}</b></div>
